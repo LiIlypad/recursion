22d195d22e9a35fa990a8472d2e2b0aff2424946
Lilly Seeley <lilly.seeley@student.allenisd.org>
2022-11-15 12:25:05 -0600

    adding files

---
+++ C101 Factorial [Swift]/.merlin/build/main.swift
@@ -0,0 +1,26 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 2
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func factorial(n:Int) -> Int{
+    var fact : Int
+    print("Calculating factorial(\(n))...")
+    if n == 1{
+        print("Terminal case, returning 1.")
+        fact = 1
+    }else{
+        fact = n * factorial(n:n-1)
+        print("Non-terminal case, returning \(fact).")
+    }
+    return fact
+}
+
+let x = factorial(n:n)
+print("The final result is: \(x).")
